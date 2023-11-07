b994086d3eb907780893dfe67c0e2696632889d7
Ilphat <ilphat.gi@gmail.com>
2023-11-07 19:02:31 +0300

    feat: add workflow to eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 
-
 function App() {
-  return (
-    <Ter>Test style components</Ter>
-  )
+  return <Ter>renders learn react link</Ter>
 }
 
 const Ter = styled.div`
