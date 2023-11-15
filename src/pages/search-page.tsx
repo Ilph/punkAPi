88938894cee35587de2cbfd2bf8d8c35ff18ac61
Ilphat <ilphat.gi@gmail.com>
2023-11-15 21:22:18 +0300

    bugfix: delete unused components

--- src/pages/search-page.tsx
+++ src/pages/search-page.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import { BaseLayoutWithHeaderFooter } from '../components/layout/base-layout-with-header-footer'
-import { MainContent } from '../components/main-content'
 import { SearchForm } from '../components/search'
 
 export const SearchPage = () => (
